7ab9397ee36ee0d3de26c942778edc90106bc662
Michael Tils <mtils@web-utils.de>
2016-06-09 12:26:39 +0200

    Fixed wrong namespace

--- src/Ems/Model/Eloquent/NotFoundException.php
+++ src/Ems/Model/Eloquent/NotFoundException.php
@@ -2,7 +2,7 @@
 
 namespace Ems\Model\Eloquent;
 
-use Ems\Core\NotFound;
+use Ems\Contracts\Core\NotFound;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class NotFoundException extends ModelNotFoundException implements NotFound {}
