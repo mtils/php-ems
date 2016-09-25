f828eba6d75fa91ceb1448cb7f9b5d2396dfbcec
Michael Tils <mtils@web-utils.de>
2016-09-25 22:15:44 +0200

    Added ems mini application classes

---
+++ src/Ems/Skeleton/BootManager.php
@@ -0,0 +1,240 @@
+<?php
+
+
+namespace Ems\Skeleton;
+
+
+use Ems\Contracts\Core\IOCContainer;
+use Ems\Core\Application as BaseApplication;
+
+/**
+ * The Bootmanager is the place where to boot your app startup classes
+ * and closures.
+ * Either you add a class to be loaded at boot via add($class) or
+ * just pass some callable to be called at boot.
+ *
+ * The Bootmanager has 3 phases while booting:
+ * 1. Package bindings
+ * 2. App bindings
+ * 3. Booting
+ *
+ * If you pass something in the provides array, the loading of the $class
+ * will be deffered until some binding of this array was requested
+ * Deferred loading is only supported by classes (add)
+ */
+class BootManager
+{
+
+    /**
+     * @var \Ems\Core\Application
+     **/
+    protected $ems;
+
+    /**
+     * @var \Ems\Contracts\Core\IOCContainer
+     **/
+    protected $container;
+
+    /**
+     * @var array
+     **/
+    protected $classes = [];
+
+    /**
+     * @var array
+     **/
+    protected $instances = [];
+
+    /**
+     * @var array
+     **/
+    protected $packageBinders = [];
+
+    /**
+     * @var array
+     **/
+    protected $binders = [];
+
+    /**
+     * @var array
+     **/
+    protected $booters = [];
+
+    /**
+     * @var array
+     **/
+    protected static $configurators = [];
+
+    /**
+     * @var bool
+     **/
+    protected $configuratorsCalled = false;
+
+    /**
+     * @param Ems\Contracts\Core\IOCContainer $container
+     **/
+    public function __construct(IOCContainer $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @return \Ems\Core\Application
+     **/
+    public function getApplication()
+    {
+        return $this->ems;
+    }
+
+    /**
+     * @param \Ems\Core\Application $ems
+     * @return self
+     **/
+    public function setApplication(BaseApplication $ems)
+    {
+        $this->ems = $ems;
+
+        $ems->booting(function($ems) {
+            $this->bindPackages($ems);
+            $this->bind($ems);
+            $this->boot($ems);
+        });
+
+        return $this;
+    }
+
+    /**
+     * Add a class to be booted / Registered. The class can have
+     * the following methods: bindPackages, bind and boot
+     * If some of the methods exists it will be called in the corresponding
+     * boot phase
+     *
+     * @param string|object $class
+     * @param array $provides (optional)
+     * @return self
+     **/
+    public function add($class, $provides=[])
+    {
+        $this->callConfiguratorsOnce();
+
+        if (is_object($class)) {
+            $this->classes[] = get_class($class);
+            $this->instances[get_class($class)] = $class;
+            return $this;
+        }
+
+        $this->classes[] = $class;
+
+        return $this;
+    }
+
+    public function addPackageBinder($name, callable $binder)
+    {
+        $this->callConfiguratorsOnce();
+        $this->packageBinders[$name] = $binder;
+        return $this;
+    }
+
+    public function addBinder($name, callable $binder)
+    {
+        $this->callConfiguratorsOnce();
+        $this->binders[$name] = $binder;
+        return $this;
+    }
+
+    public function addBooter($name, callable $booter)
+    {
+        $this->callConfiguratorsOnce();
+        $this->booters[$name] = $booter;
+        return $this;
+    }
+
+    public function bindPackages(IOCContainer $container)
+    {
+        $this->callAll('bindPackages', $this->packageBinders);
+    }
+
+    public function bind(IOCContainer $container)
+    {
+        $this->callAll('bind', $this->binders);
+    }
+
+    public function boot(IOCContainer $container)
+    {
+        $this->callAll('boot', $this->booters);
+    }
+
+    /**
+     * Do something before the first add/addBinder/... call
+     * is invoked. The instantiated BootManager and the
+     * container will be passed
+     *
+     * @param callable $listener
+     * @return void
+     **/
+    public static function configureBy(callable $listener)
+    {
+        static::$configurators[] = $listener;
+    }
+
+    protected function callAll($method, array $callables)
+    {
+
+        $this->callConfiguratorsOnce();
+
+        foreach ($this->classes as $class) {
+            $this->callIfExists($this->resolveOnce($class), $method);
+        }
+        foreach ($callables as $name=>$binder) {
+            $this->container->call($binder, [$this->container]);
+        }
+    }
+
+    /**
+     * Resolves the boot class once via the container
+     *
+     * @param string $class
+     * @return object
+     **/
+    protected function resolveOnce($class)
+    {
+        if (isset($this->instances[$class])) {
+            return $this->instances[$class];
+        }
+        $this->instances[$class] = call_user_func($this->container, $class);
+        return $this->instances[$class];
+    }
+
+    /**
+     * Calls the booter method if exists
+     *
+     * @param object $booter
+     * @param string $method
+     * @return null
+     **/
+    protected function callIfExists($booter, $method)
+    {
+        if (method_exists($booter, $method)) {
+            $this->container->call([$booter, $method], [$this->container]);
+        }
+    }
+
+    /**
+     * Calls the creation listeners
+     *
+     * @return null
+     **/
+    protected function callConfiguratorsOnce()
+    {
+        if ($this->configuratorsCalled) {
+            return;
+        }
+
+        foreach (static::$configurators as $listener) {
+            call_user_func($listener, $this, $this->container);
+        }
+
+        $this->configuratorsCalled = true;
+    }
+
+}
---
+++ tests/integration/Skeleton/BootManagerTest.php
@@ -0,0 +1,116 @@
+<?php
+
+
+namespace Ems\Skeleton;
+use Ems\Testing\LoggingCallable;
+use Ems\Testing\Cheat;
+
+class BootManagerTest extends \Ems\IntegrationTest
+{
+
+    public function test_it_calls_configurators_once_before_adding()
+    {
+        $manager = $this->newBootManager();
+        $booter = new LoggingCallable;
+        BootManager::configureBy($booter);
+
+        $this->assertCount(0, $booter);
+        $manager->add('stdClass');
+        $this->assertCount(1, $booter);
+        $manager->add('stdClass');
+        $this->assertCount(1, $booter);
+
+    }
+
+    public function test_it_hooks_into_boot_process()
+    {
+        $manager = $this->newBootManager();
+        $app = $this->app();
+
+        $manager->setApplication($app);
+        $this->assertSame($app, $manager->getApplication());
+
+        $packageBinder = new LoggingCallable;
+        $binder = new LoggingCallable;
+        $booter = new LoggingCallable;
+        $callables = [$packageBinder, $binder, $booter];
+
+        $manager->add(BootManagerTestBooter::class);
+        $manager->addPackageBinder('package', $packageBinder);
+        $manager->addBinder('bind', $binder);
+        $manager->addBooter('boot', $booter);
+
+        $this->assertAllCount(0, $callables);
+
+        $app->boot();
+
+        $this->assertAllCount(1, $callables);
+
+        $object = Cheat::call($manager, 'resolveOnce', [BootManagerTestBooter::class]);
+
+        $this->assertEquals(1, $object->packageCalled);
+        $this->assertEquals(1, $object->bindCalled);
+        $this->assertEquals(1, $object->bootCalled);
+
+    }
+
+    public function test_it_boots_passed_object()
+    {
+        $manager = $this->newBootManager();
+        $app = $this->app();
+
+        $manager->setApplication($app);
+        $this->assertSame($app, $manager->getApplication());
+
+
+        $booter = new BootManagerTestBooter;
+        $manager->add($booter);
+
+        $app->boot();
+
+        $this->assertEquals(1, $booter->packageCalled);
+        $this->assertEquals(1, $booter->bindCalled);
+        $this->assertEquals(1, $booter->bootCalled);
+
+        $this->assertSame($booter, Cheat::call($manager, 'resolveOnce', [BootManagerTestBooter::class]));
+
+    }
+
+    protected function assertAllCount($count, $countables)
+    {
+        foreach ($countables as $countable) {
+            $this->assertCount($count, $countable);
+        }
+    }
+
+    protected function newBootManager()
+    {
+        return new BootManager($this->app());
+    }
+}
+
+class BootManagerTestBooter
+{
+
+    public $packageCalled = 0;
+
+    public $bindCalled = 0;
+
+    public $bootCalled = 0;
+
+
+    public function bindPackages()
+    {
+        $this->packageCalled++;
+    }
+
+    public function bind()
+    {
+        $this->bindCalled++;
+    }
+
+    public function boot()
+    {
+        $this->bootCalled++;
+    }
+}
