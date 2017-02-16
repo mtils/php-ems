0b979361b644cea82cc4eec6d91a0353138d91fe
Michael Tils <mtils@web-utils.de>
2017-02-16 17:32:57 +0100

    Added extendable by class hierarchy trait

---
+++ src/Ems/Core/Patterns/ExtendableByClassHierarchyTrait.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace Ems\Core\Patterns;
+
+use Ems\Core\Exceptions\HandlerNotFoundException;
+use Ems\Core\Helper;
+use OutOfBoundsException;
+
+/**
+ * This trait also implements the Extendable interface but searches handlers
+ * by a class hierarchy.
+ * You could have this hierarchy:
+ *
+ * class Model
+ * class User extends Model
+ * class CmsUser extends User
+ *
+ * Then you could call $obj->extend(Model::class, $callable)
+ * And this callable will be called on Model, User and CmsUser.
+ * Until you call $obj->extend(User::class, $callable), because User is nearer
+ * then Model.
+ *
+ * @see \Ems\Contracts\Core\Extendable
+ **/
+trait ExtendableByClassHierarchyTrait
+{
+    /**
+     * Here the callables are held.
+     *
+     * @var array
+     **/
+    protected $_extensions = [];
+
+    /**
+     * @var array
+     **/
+    protected $_cache = [];
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param string   $name
+     * @param callable $callable
+     *
+     * @return self
+     **/
+    public function extend($name, callable $callable)
+    {
+        $this->_extensions[$name] = $callable;
+
+        return $this;
+    }
+
+    /**
+     * Return the extension named $name.
+     *
+     * @param string $name
+     *
+     * @return mixed
+     **/
+    public function getExtension($name)
+    {
+        if ($extension = $this->nearestForClass($name)) {
+            return $extension;
+        }
+
+        throw new OutOfBoundsException(get_class($this).": No extension named \"$name\" found");
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return array
+     **/
+    public function extensions()
+    {
+        return array_keys($this->_extensions);
+    }
+
+    /**
+     * This method finds the best matching callable for class $class.
+     * It decorates findNearestForClass to cache the result
+     *
+     * @param string $findClass
+     * @param string $type (optional)
+     * @return callable|null
+     **/
+    protected function nearestForClass($findClass)
+    {
+
+        if (isset($this->_cache[$findClass])) {
+            return $this->_cache[$findClass];
+        }
+
+        if (!count($this->_extensions)) {
+            return;
+        }
+
+        if (isset($this->_extensions[$findClass])) {
+            $this->_cache[$findClass] = $this->_extensions[$findClass];
+            return $this->_cache[$findClass];
+        }
+
+        if (!$nearest = $this->findNearestForClass($this->_extensions, $findClass)) {
+            return;
+        }
+
+        $this->_cache[$findClass] = $nearest;
+
+        return $nearest;
+
+    }
+
+    /**
+     * This method does the actual work to find the best matching callable for
+     * class $class
+     *
+     * @param array $providers
+     * @param string $findClass
+     * @return callable|null
+     **/
+    protected function findNearestForClass(&$providers, $findClass)
+    {
+
+        if (!$all = $this->findAllForClass($providers, $findClass)) {
+            return;
+        }
+
+        if (count($all) == 1) {
+            return array_values($all)[0];
+        }
+
+        foreach ($this->inheritance($findClass) as $parentClass) {
+            if (isset($all[$parentClass]) ) {
+                return $all[$parentClass];
+            }
+        }
+
+    }
+
+    /**
+     * Returns all providers which are assigned for $findClass or one of its
+     * parent classes
+     *
+     * @param array $providers
+     * @param string $findClass
+     * @return array
+     **/
+    protected function findAllForClass(&$providers, $findClass)
+    {
+
+        $all = [];
+
+        foreach ($providers as $class=>$provider) {
+            if (is_subclass_of($findClass, $class) || $findClass == $class) {
+                $all[$class] = $provider;
+            }
+        }
+
+        return $all;
+
+    }
+
+    /**
+     * Call the extension named $name with $params.
+     *
+     * @param string $name
+     * @param mixed $params (optional)
+     *
+     * @return mixed
+     **/
+    protected function callExtension($name, $params = [])
+    {
+        return Helper::call($this->getExtension($name), $params);
+    }
+
+    /**
+     * Return the class inheritance of $class
+     *
+     * @param string $class
+     *
+     * @return array
+     **/
+    protected function inheritance($class)
+    {
+        $parents = class_parents($class, false);
+        return [$class] + $parents;
+    }
+}
---
+++ tests/unit/Core/Patterns/ExtendableByClassHierarchyTraitTest.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Ems\Core\Patterns;
+
+use Ems\Contracts\Core\Extendable;
+use Ems\Testing\LoggingCallable;
+
+class ExtendableByClassHierarchyTraitTest extends \Ems\TestCase
+{
+    public function test_implements_interface()
+    {
+        $this->assertInstanceOf(
+            Extendable::class,
+            $this->newExtendable()
+        );
+    }
+
+    public function test_getExtension_returns_direct_match()
+    {
+
+        $object = $this->newExtendable();
+
+        $ext = new Extension('a');
+
+        $object->extend(Base::class, $ext);
+
+        $this->assertSame($ext, $object->getExtension(Base::class));
+
+    }
+
+    public function test_getExtension_returns_match_of_parent_class()
+    {
+
+        $object = $this->newExtendable();
+
+        $ext = new Extension('a');
+
+        $object->extend(Base::class, $ext);
+
+        $this->assertSame($ext, $object->getExtension(ExtendedBase::class));
+
+        $this->assertSame($ext, $object->getExtension(ExtendedBase::class));
+
+    }
+
+    public function test_getExtension_returns_right_extension_per_hierarchy()
+    {
+
+        $object = $this->newExtendable();
+
+        $baseExt = new Extension('a');
+        $extendedExt = new Extension('b');
+
+        $object->extend(Base::class, $baseExt);
+        $object->extend(ExtendedBase::class, $extendedExt);
+
+        $this->assertSame($baseExt, $object->getExtension(Base::class));
+        $this->assertSame($extendedExt, $object->getExtension(ExtendedBase::class));
+        $this->assertSame($extendedExt, $object->getExtension(EvenMoreExtendedBase::class));
+    }
+
+    public function test_getExtension_returns_right_extension_per_hierarchy2()
+    {
+
+        $object = $this->newExtendable();
+
+        $baseExt = new Extension('a');
+        $extendedExt = new Extension('b');
+
+        $object->extend(Base::class, $baseExt);
+        $object->extend(EvenMoreExtendedBase::class, $extendedExt);
+
+        $this->assertSame($baseExt, $object->getExtension(Base::class));
+        $this->assertSame($baseExt, $object->getExtension(ExtendedBase::class));
+        $this->assertSame($extendedExt, $object->getExtension(EvenMoreExtendedBase::class));
+
+
+    }
+
+    public function test_getExtensions_returns_assigned_classes()
+    {
+
+        $object = $this->newExtendable();
+
+        $baseExt = new Extension('a');
+        $extendedExt = new Extension('b');
+
+        $object->extend(Base::class, $baseExt);
+        $object->extend(EvenMoreExtendedBase::class, $extendedExt);
+
+        $this->assertEquals([Base::class, EvenMoreExtendedBase::class], $object->extensions());
+
+
+    }
+
+    /**
+     * @expectedException OutOfBoundsException
+     **/
+    public function test_getExtension_throws_OutOfBoundsException_if_extension_not_found()
+    {
+
+        $object = $this->newExtendable();
+
+        $baseExt = new Extension('a');
+        $extendedExt = new Extension('b');
+
+        $object->extend(Base::class, $baseExt);
+        $object->extend(EvenMoreExtendedBase::class, $extendedExt);
+
+        $object->getExtension('stdClass');
+
+    }
+
+    /**
+     * @expectedException OutOfBoundsException
+     **/
+    public function test_getExtension_throws_OutOfBoundsException_if_no_extensions_assigned()
+    {
+
+        $object = $this->newExtendable();
+
+        $object->getExtension('stdClass');
+
+    }
+
+    /**
+     * @expectedException OutOfBoundsException
+     **/
+    public function test_getExtension_throws_OutOfBoundsException_if_no_matching_extension()
+    {
+
+        $object = $this->newExtendable();
+
+        $baseExt = new Extension('a');
+
+        $object->extend(stdClass::class, $baseExt);
+        $object->extend(static::class, $baseExt);
+
+        $object->getExtension(EvenMoreExtendedBase::class);
+
+    }
+
+    protected function newExtendable()
+    {
+        return new ExtendableByClassHierarchyTraitTestObject();
+    }
+
+}
+
+class ExtendableByClassHierarchyTraitTestObject implements Extendable
+{
+    use ExtendableByClassHierarchyTrait;
+}
+
+class Base{}
+
+class ExtendedBase extends Base {}
+
+class EvenMoreExtendedBase extends ExtendedBase {}
+
+class Extension
+{
+    public $name = 'a';
+
+    public function __construct($name='a')
+    {
+        $this->name = $name;
+    }
+
+    public function __invoke()
+    {
+    }
+}
