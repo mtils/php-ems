882db9bfccb801b8500ae02962bd80e9da1d0ef3
Michael Tils <mtils@web-utils.de>
2017-03-06 14:37:55 +0100

    Make TypeProvider interface more equal to Extractor

--- src/Ems/Contracts/XType/TypeProvider.php
+++ src/Ems/Contracts/XType/TypeProvider.php
@@ -5,12 +5,13 @@ namespace Ems\Contracts\XType;
 interface TypeProvider
 {
     /**
-     * Returns a xtype object for an object property.
+     * Returns a xtype object for an object property. If path is null return the
+     * Xtype for the whole class
      *
-     * @param string|object $resource The resource name, class or an object of it
-     * @param string        $path     A key name. Can be dotted like address.street.name
+     * @param mixed   $resource The resource name, class, an object of it or just some variable
+     * @param string  $path     (optional) A key name. Can be dotted like address.street.name
      *
-     * @return \Ems\Contracts\Xtype\XType
+     * @return XType
      **/
-    public function keyType($resource, $path);
+    public function xType($root, $path=null);
 }
