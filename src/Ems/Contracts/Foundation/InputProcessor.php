92cf5811d2e3c42c098508a52ebeb923993e022d
Michael Tils <mtils@web-utils.de>
2017-06-15 16:07:23 +0200

    Added InputProcessor

---
+++ src/Ems/Contracts/Foundation/InputProcessor.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ems\Contracts\Foundation;
+
+
+use Ems\Contracts\Core\NamedCallableChain;
+use Ems\Contracts\Core\AppliesToResource;
+
+/**
+ * An input processor sorts/casts/corrects input to work with it inside your
+ * application.
+ * This is generally an approach to remove code from controllers.
+ **/
+interface InputProcessor extends NamedCallableChain
+{
+    /**
+     * Sort. rearrange, cast, whatever do with the input array
+     *
+     * @param array             $input
+     * @param AppliesToResource $resource (optional)
+     *
+     * @return array
+     **/
+    public function process(array $input, AppliesToResource $resource=null);
+}
---
+++ src/Ems/Foundation/InputProcessor.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Ems\Foundation;
+
+use Ems\Contracts\Core\AppliesToResource;
+use Ems\Contracts\Foundation\InputProcessor as InputProcessorContract;
+use Ems\Core\Support\ProvidesNamedCallableChain;
+
+class InputProcessor implements InputProcessorContract
+{
+    use ProvidesNamedCallableChain;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param array             $input
+     * @param AppliesToResource $resource (optional)
+     *
+     * @return array
+     **/
+    public function process(array $input, AppliesToResource $resource=null)
+    {
+        $corrected = $input;
+
+        foreach ($this->buildChain() as $name => $parameters) {
+            $corrector = $this->getExtension($name);
+            $result = $corrector($corrected, $resource);
+            $corrected = &$result;
+        }
+
+        return $corrected;
+    }
+
+}
