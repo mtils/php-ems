660047f29f3b0ebae8c5d80fc166fbb9a8f83e94
Michael Tils <mtils@web-utils.de>
2017-07-07 22:02:11 +0200

    More exact docs

--- src/Ems/Contracts/Core/AppliesToResource.php
+++ src/Ems/Contracts/Core/AppliesToResource.php
@@ -3,25 +3,17 @@
 namespace Ems\Contracts\Core;
 
 /**
- * This interface is one of the fundamentals of this framework.
- * This fundamental is: "Care at this point of code about what this point is caring about"
- * So for example a controller should not deceide everything concerning the action it processes.
- * For example a web page registration. Much things are happening during a registration:
- * Activation codes, tokens, emails, templates, messages,....
- * To allow to defer this things to other places I introduced that defined endpoints for your
- * application actions are defined like in REST and all the (service) objects are now acting
- * based on that resource name. (Mail::mailFor('registration.create'))
- * So the control is outside of the controller.
+ * A resource is anything that's important enough to be referenced as a thing
+ * itself (in your application).
+ * EMS takes the mostly used naming convention in REST applications: plural
+ * minus lowercase names.
+ * So if you have an entity of class App\User its resourceName would be users.
+ * An entity named App\Blog\BlogEntry would be named blog-entries.
  **/
 interface AppliesToResource
 {
     /**
-     * Return the resource name. A resource name is a locator for your
-     * application endpoint. Its very similar to a REST endpoint.
-     * A resource name is a resource endpoint with an action
-     * with dots instead of (url) slashes.
-     * For example: 'users.index' (url would be /users),
-     * 'mail.attachments.create' (url would be /mail/136/attachments/create).
+     * Return the resource name. See class doc.
      *
      * @return string
      **/
