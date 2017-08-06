90f97bd8f65779dce4457e2e542bc0dac973e4fd
Michael Tils <mtils@web-utils.de>
2017-08-06 14:19:08 +0200

    Added new CsvReadIterator for magically simple CSV reading

---
+++ src/Ems/Core/Filesystem/CsvReadIterator.php
@@ -0,0 +1,429 @@
+<?php
+
+namespace Ems\Core\Filesystem;
+
+use Ems\Contracts\Core\Filesystem;
+use Ems\Contracts\Core\ContentIterator;
+use Ems\Core\LocalFilesystem;
+use Ems\Core\Helper;
+
+/**
+ * The CsvReadIterator is an iterator which allows to read
+ * csv files row by row.
+ * If you do not set headers or set any separators/delimiters
+ * it tries to guess it via the CsvDetector.
+ *
+ * @sample foreach (new CsvReadIterator($file) as $row) ...
+ *
+ * Definitions used in this class:
+ *
+ * row: One row of data. The header is excluded, so row[0] is never the header
+ * header: an indexed array of column names.
+ * separator: The column separator (,;|)
+ * delimiter: The string delimiter if strings contain the separator or \n
+ **/
+class CsvReadIterator implements ContentIterator
+{
+    use ReadIteratorTrait;
+
+    /**
+     * @var string
+     **/
+    protected $separator = '';
+
+    /**
+     * @var string
+     **/
+    protected $delimiter = '"';
+
+    /**
+     * @var array
+     **/
+    protected $header = [];
+
+    /**
+     * Caching of header presence
+     *
+     * @var bool
+     **/
+    protected $hasHeader = false;
+
+    /**
+     * @var bool
+     **/
+    private $headerRowSkipped = false;
+
+    /**
+     * @var bool
+     **/
+    private $separatorWasDetected = false;
+
+    /**
+     * @var bool
+     **/
+    private $headerWasDetected = false;
+
+    /**
+     * @var int
+     **/
+    protected $startAtRow;
+
+    /**
+     * @var CsvDetector
+     **/
+    protected $detector;
+
+    /**
+     * @var LineReadIterator
+     **/
+    protected $lineReader;
+
+    /**
+     * @var string
+     **/
+    protected $firstLines;
+
+    /**
+     * @var self
+     **/
+    protected $countInstance;
+
+    /**
+     * @param string     $filePath   (optional)
+     * @param Filesystem $filesystem (optional)
+     **/
+    public function __construct($filePath = '', Filesystem $filesystem = null, CsvDetector $detector = null, LineReadIterator $lineReader = null)
+    {
+        $this->position = 0;
+        $this->filePath = $filePath;
+        $this->chunkSize = 0;
+        $this->setFilesystem($filesystem ?: new LocalFilesystem());
+        $this->setDetector($detector ?: new CsvDetector());
+        $this->setLineReader($lineReader ?: new LineReadIterator());
+    }
+
+    /**
+     * Return the column separator
+     *
+     * @return string
+     **/
+    public function getSeparator()
+    {
+        if (!$this->separator && $this->getFilePath()) {
+            $this->separator = $this->detector->separator(
+                $this->firstLines(),
+                $this->getDelimiter()
+            );
+            $this->separatorWasDetected = true;
+        }
+
+        return $this->separator;
+    }
+
+    /**
+     * Set the column separator sign
+     *
+     * @param string $separator
+     *
+     * @return self
+     **/
+    public function setSeparator($separator)
+    {
+        $this->separator = $separator;
+        return $this;
+    }
+
+    /**
+     * Get the string delimiter (for strings containing newlines or separators)
+     *
+     * @return string
+     **/
+    public function getDelimiter()
+    {
+        return $this->delimiter;
+    }
+
+    /**
+     * Set the string delimiter (for strings containing newlines or separators)
+     *
+     * @param string $delimiter
+     *
+     * @return self
+     **/
+    public function setDelimiter($delimiter)
+    {
+        $this->delimiter = $delimiter;
+        return $this;
+    }
+
+    /**
+     * @return CsvDetector
+     **/
+    public function getDetector()
+    {
+        return $this->detector;
+    }
+
+    /**
+     * @param CsvDetector $detector
+     *
+     * @return self
+     **/
+    public function setDetector(CsvDetector $detector)
+    {
+        $this->detector = $detector;
+        return $this;
+    }
+
+    /**
+     * @return LineReadIterator
+     **/
+    public function getLineReader()
+    {
+        return $this->lineReader;
+    }
+
+    /**
+     * @param LineReadIterator $lineReader
+     *
+     * @return self
+     **/
+    public function setLineReader(LineReadIterator $lineReader)
+    {
+        $this->lineReader = $lineReader;
+        if ($this->filesystem) {
+            $this->lineReader->setFilesystem($this->filesystem);
+        }
+        if ($this->filePath) {
+            $this->lineReader->setFilePath($this->filePath);
+        }
+        return $this;
+    }
+
+    /**
+     * Return the csv header. A header is an array of column names ['id', 'name'...].
+     *
+     * @return array
+     **/
+    public function getHeader()
+    {
+        if ($this->hasHeader) {
+            return $this->header;
+        }
+
+        if (!$this->getFilePath()) {
+            return $this->header;
+        }
+
+        $header = $this->detector->header(
+            $this->firstLines(),
+            $this->getSeparator(),
+            $this->getDelimiter()
+        );
+
+        $this->headerWasDetected = !$this->isNumericHeader($header);
+
+        $this->setHeader($this->headerWasDetected ? $header : []);
+
+        return $this->header;
+    }
+
+    /**
+     * Set the csv file header. An header is a indexed array of column
+     * names ['id', 'name'].
+     * Set no header or an empty array to let CsvDetector detect the
+     * header.
+     * If an header was set (count($this->header) the first line
+     * will automatically skipped
+     *
+     * @param array $header
+     *
+     * @return self
+     **/
+    public function setHeader(array $header)
+    {
+        $this->header = $header;
+        $this->hasHeader = (bool)count($header);
+        return $this;
+    }
+
+    /**
+     * Return the row count of the file
+     *
+     * @return int
+     **/
+    public function count()
+    {
+        foreach ($this->newCountInstance() as $i=>$row) {
+        }
+        return $i+1;
+    }
+
+    /**
+     * Read the next row and return it. Skip empty lines.
+     *
+     * @param resource $handle
+     * @param int      $chunkSize
+     *
+     * @return array|null
+     **/
+    protected function readNext($handle, $chunkSize)
+    {
+        if (feof($handle)) {
+            return;
+        }
+
+        $row = $this->readRow($handle, $chunkSize);
+
+        if (!$this->startAtRow && $this->hasHeader && !$this->headerRowSkipped) {
+            $this->headerRowSkipped = true;
+            return $this->readNext($handle, $chunkSize);
+        }
+
+        return $row === [] ? $this->readNext($handle, $chunkSize) : $row;
+    }
+
+    /**
+     * Read the next row from the file
+     *
+     * @param resource $handle
+     * @param int      $chunkSize (ignored)
+     *
+     * @return array
+     **/
+    protected function readRow($handle, $chunkSize)
+    {
+        $row = str_getcsv($this->readLine($handle, 0), $this->separator, $this->delimiter);
+
+        if ($this->isSkippableRow($row)) {
+            return [];
+        }
+
+        if (!$this->hasHeader) {
+            return $row;
+        }
+
+        $namedRow = [];
+
+        foreach ($this->header as $i=>$column) {
+            $namedRow[$column] = $row[$i];
+        }
+
+        return $namedRow;
+    }
+
+    /**
+     * A skippable row is a (by php) empty row or a row containing no data. This
+     * happens often with spreadsheet programs like excel. Here you often have
+     * ;;;;;;;;;;;; lines because excel cant determine the end of the file
+     * correctly.
+     *
+     * @param array $row
+     *
+     * @return bool
+     **/
+    protected function isSkippableRow(array $row)
+    {
+        // PHP handles empty LINES (just a \n) with a special array...i love php
+        if ( (count($row) == 1) && ($row[0] == null) ) {
+            return true;
+        }
+
+        // Test if any of the values is not trimmed ""
+        foreach ($row as $value) {
+            if (trim($value) !== '') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Empty first line cache if file did change
+     **/
+    protected function onFileChanged()
+    {
+        $this->firstLines = null;
+        if ($this->separatorWasDetected) {
+            $this->separator = '';
+            $this->separatorWasDetected = false;
+        }
+        if ($this->headerWasDetected) {
+            $this->setHeader([]);
+            $this->headerWasDetected = false;
+        }
+    }
+
+    /**
+     * Empty first line cache if file did change
+     **/
+    protected function onRewind()
+    {
+        $this->headerRowSkipped = false;
+        // Trigger detection once
+        $this->getHeader();
+    }
+
+    /**
+     * Return the first lines of the input file
+     *
+     * @param int $lineCount
+     *
+     * @return string
+     **/
+    protected function firstLines($lineCount=20)
+    {
+        if ($this->firstLines !== null) {
+            return $this->firstLines;
+        }
+
+        $this->lineReader->setFilePath($this->filePath);
+
+        $lines = [];
+
+        foreach ($this->lineReader as $i=>$line) {
+            $lines[] = $line;
+
+            if ($i >= $lineCount) {
+                break;
+            }
+        }
+
+        $this->lineReader->releaseHandle();
+
+        $this->firstLines = implode("\n", $lines);
+
+        return $this->firstLines;
+    }
+
+    /**
+     * Returns if the passed header is a numeric default header of CsvDetector
+     * if it can detect the amount of columns but no names
+     *
+     * @param array $header
+     *
+     * @return bool
+     **/
+    protected function isNumericHeader($header)
+    {
+        return $header == range(0, count($header)-1);
+    }
+
+    /**
+     * Create an instance of this reader just for the line count
+     *
+     * @return self
+     **/
+    protected function newCountInstance()
+    {
+        $instance = new static(
+            $this->getFilePath(),
+            $this->getFilesystem(),
+            $this->getDetector(),
+            clone $this->getLineReader()
+        );
+        return $instance->setDelimiter($this->getDelimiter())
+                        ->setSeparator($this->getSeparator())
+                        ->setHeader($this->getHeader());
+    }
+}
---
+++ tests/integration/Core/Filesystem/CsvReadIteratorTest.php
@@ -0,0 +1,347 @@
+<?php
+
+namespace Ems\Core\Filesystem;
+
+use Ems\Contracts\Core\Filesystem as FSContract;
+use Ems\Contracts\Core\ContentIterator;
+use Ems\Core\LocalFilesystem;
+use Ems\Testing\FilesystemMethods;
+
+class CsvReadIteratorTest extends \Ems\IntegrationTest
+{
+
+    public function test_implements_interface()
+    {
+        $this->assertInstanceOf(
+            ContentIterator::class,
+            $this->newReader()
+        );
+    }
+
+    public function test_getSeparator_detects_separator_if_non_setted()
+    {
+        $reader = $this->newReader($this->dataFile('simple-pipe-placeholder-normalized.csv'));
+        $this->assertEquals('|', $reader->getSeparator());
+    }
+
+    public function test_getSeparator_returns_setted_separator()
+    {
+        $reader = $this->newReader($this->dataFile('simple-pipe-placeholder-normalized.csv'));
+        $this->assertSame($reader, $reader->setSeparator(';'));
+        $this->assertEquals(';', $reader->getSeparator());
+    }
+
+    public function test_setDelimiter_sets_delimiter()
+    {
+        $reader = $this->newReader($this->dataFile('simple-pipe-placeholder-normalized.csv'));
+        $this->assertSame($reader, $reader->setDelimiter("'"));
+        $this->assertEquals("'", $reader->getDelimiter());
+    }
+
+    public function test_getDetector_returns_Detector()
+    {
+        $this->assertInstanceOf(CsvDetector::class, $this->newReader()->getDetector());
+    }
+
+    public function test_getLineReader_returns_LineReadIterator()
+    {
+        $this->assertInstanceOf(LineReadIterator::class, $this->newReader()->getLineReader());
+    }
+
+    public function test_getHeader_detects_header_if_not_setted()
+    {
+        $reader = $this->newReader($this->dataFile('simple-pipe-placeholder-normalized.csv'));
+        $this->assertEquals(
+            ['id', 'name', 'last_name', 'age', 'street'],
+            $reader->getHeader()
+        );
+    }
+
+    public function test_getHeader_returns_setted_header()
+    {
+        $reader = $this->newReader();
+        $this->assertSame($reader, $reader->setHeader(['id', 'name']));
+        $this->assertEquals(['id', 'name'], $reader->getHeader());
+    }
+
+    public function test_getHeader_returns_empty_header_if_no_filePath_setted()
+    {
+        $reader = $this->newReader();
+        $this->assertEquals([], $reader->getHeader());
+    }
+
+    public function test_read_simple_csv_file()
+    {
+        $reader = $this->newReader($this->dataFile('simple-pipe-placeholder-normalized.csv'));
+
+        $result = [];
+
+        foreach ($reader as $row) {
+            $result[] = $row;
+        }
+
+        $awaited = [
+            [
+                'id'        => '42',
+                'name'      => 'Talent',
+                'last_name' => 'Billy',
+                'age'       => '35',
+                'street'    => 'Elm Street'
+            ],
+            [
+                'id'        => '52',
+                'name'      => 'Duck',
+                'last_name' => 'Donald',
+                'age'       => '8',
+                'street'    => 'Duckcity'
+            ]
+        ];
+        $this->assertEquals($awaited, $result);
+
+    }
+
+    public function test_read_simple_csv_file_when_no_header_setted()
+    {
+        $reader = $this->newReader($this->dataFile('simple-pipe-placeholder-no-header.csv'));
+
+        $result = [];
+
+        foreach ($reader as $row) {
+            $result[] = $row;
+        }
+
+        $awaited = [
+            [
+                0        => '42',
+                1      => 'Talent',
+                2 => 'Billy',
+                3       => '35',
+                4    => 'Elm Street'
+            ],
+            [
+                0        => '52',
+                1      => 'Duck',
+                2   => 'Donald',
+                3       => '8',
+                4    => 'Duckcity'
+            ]
+        ];
+
+        $this->assertEquals($awaited, $result);
+
+    }
+
+    public function test_read_country_csv_file()
+    {
+        $reader = $this->newReader($this->dataFile('Countries-ISO-3166-2.csv'));
+
+        $firstRowShouldBe = [
+            'Sort Order'               => '1',
+            'Common Name'              => 'Afghanistan',
+            'Formal Name'              => 'Islamic State of Afghanistan',
+            'Type'                     => 'Independent State',
+            'Sub Type'                 => '',
+            'Sovereignty'              => '',
+            'Capital'                  => 'Kabul',
+            'ISO 4217 Currency Code'   => 'AFN',
+            'ISO 4217 Currency Name'   => 'Afghani',
+            'ITU-T Telephone Code'     => '93',
+            'ISO 3166-1 2 Letter Code' => 'AF',
+            'ISO 3166-1 3 Letter Code' => 'AFG',
+            'ISO 3166-1 Number'        => '4',
+            'IANA Country Code TLD'    => '.af'
+        ];
+
+        $lastRowShouldBe = [
+            'Sort Order'               => '272',
+            'Common Name'              => 'British Antarctic Territory',
+            'Formal Name'              => '',
+            'Type'                     => 'Antarctic Territory',
+            'Sub Type'                 => 'Overseas Territory',
+            'Sovereignty'              => 'United Kingdom',
+            'Capital'                  => '',
+            'ISO 4217 Currency Code'   => '',
+            'ISO 4217 Currency Name'   => '',
+            'ITU-T Telephone Code'     => '',
+            'ISO 3166-1 2 Letter Code' => 'AQ',
+            'ISO 3166-1 3 Letter Code' => 'ATA',
+            'ISO 3166-1 Number'        => '10',
+            'IANA Country Code TLD'    => '.aq'
+        ];
+
+        $firstRow = [];
+
+        foreach ($reader as $i=>$row) {
+            if ($i==0) {
+                $firstRow = $row;
+            }
+        }
+
+        $this->assertEquals($firstRowShouldBe, $firstRow);
+        $this->assertEquals($lastRowShouldBe, $row);
+        $this->assertEquals(268, $i+1);
+
+    }
+
+    public function test_read_country_csv_file_with_skippable_lines()
+    {
+        $reader = $this->newReader($this->dataFile('Countries-ISO-3166-2-semicolon-blank-lines.csv'));
+
+        $firstRowShouldBe = [
+            'Sort Order'               => '1',
+            'Common Name'              => 'Afghanistan',
+            'Formal Name'              => 'Islamic State of Afghanistan',
+            'Type'                     => 'Independent State',
+            'Sub Type'                 => '',
+            'Sovereignty'              => '',
+            'Capital'                  => 'Kabul',
+            'ISO 4217 Currency Code'   => 'AFN',
+            'ISO 4217 Currency Name'   => 'Afghani',
+            'ITU-T Telephone Code'     => '93',
+            'ISO 3166-1 2 Letter Code' => 'AF',
+            'ISO 3166-1 3 Letter Code' => 'AFG',
+            'ISO 3166-1 Number'        => '4',
+            'IANA Country Code TLD'    => '.af'
+        ];
+
+        $lastRowShouldBe = [
+            'Sort Order'               => '272',
+            'Common Name'              => 'British Antarctic Territory',
+            'Formal Name'              => '',
+            'Type'                     => 'Antarctic Territory',
+            'Sub Type'                 => 'Overseas Territory',
+            'Sovereignty'              => 'United Kingdom',
+            'Capital'                  => '',
+            'ISO 4217 Currency Code'   => '',
+            'ISO 4217 Currency Name'   => '',
+            'ITU-T Telephone Code'     => '',
+            'ISO 3166-1 2 Letter Code' => 'AQ',
+            'ISO 3166-1 3 Letter Code' => 'ATA',
+            'ISO 3166-1 Number'        => '10',
+            'IANA Country Code TLD'    => '.aq'
+        ];
+
+        $firstRow = [];
+
+        foreach ($reader as $i=>$row) {
+            if ($i==0) {
+                $firstRow = $row;
+            }
+        }
+
+        $this->assertEquals($firstRowShouldBe, $firstRow);
+        $this->assertEquals($lastRowShouldBe, $row);
+        $this->assertEquals(268, $i+1);
+
+    }
+
+    public function test_setting_new_file_on_same_reader()
+    {
+        $reader = $this->newReader($this->dataFile('Countries-ISO-3166-2.csv'));
+
+        $firstRowShouldBe = [
+            'Sort Order'               => '1',
+            'Common Name'              => 'Afghanistan',
+            'Formal Name'              => 'Islamic State of Afghanistan',
+            'Type'                     => 'Independent State',
+            'Sub Type'                 => '',
+            'Sovereignty'              => '',
+            'Capital'                  => 'Kabul',
+            'ISO 4217 Currency Code'   => 'AFN',
+            'ISO 4217 Currency Name'   => 'Afghani',
+            'ITU-T Telephone Code'     => '93',
+            'ISO 3166-1 2 Letter Code' => 'AF',
+            'ISO 3166-1 3 Letter Code' => 'AFG',
+            'ISO 3166-1 Number'        => '4',
+            'IANA Country Code TLD'    => '.af'
+        ];
+
+        $lastRowShouldBe = [
+            'Sort Order'               => '272',
+            'Common Name'              => 'British Antarctic Territory',
+            'Formal Name'              => '',
+            'Type'                     => 'Antarctic Territory',
+            'Sub Type'                 => 'Overseas Territory',
+            'Sovereignty'              => 'United Kingdom',
+            'Capital'                  => '',
+            'ISO 4217 Currency Code'   => '',
+            'ISO 4217 Currency Name'   => '',
+            'ITU-T Telephone Code'     => '',
+            'ISO 3166-1 2 Letter Code' => 'AQ',
+            'ISO 3166-1 3 Letter Code' => 'ATA',
+            'ISO 3166-1 Number'        => '10',
+            'IANA Country Code TLD'    => '.aq'
+        ];
+
+        $firstRow = [];
+
+        foreach ($reader as $i=>$row) {
+            if ($i==0) {
+                $firstRow = $row;
+            }
+        }
+
+        $this->assertEquals($firstRowShouldBe, $firstRow);
+        $this->assertEquals($lastRowShouldBe, $row);
+        $this->assertEquals(268, $i+1);
+
+        $reader->setFilePath($this->dataFile('simple-pipe-placeholder-normalized.csv'));
+
+        $result = [];
+
+        foreach ($reader as $row) {
+            $result[] = $row;
+        }
+
+        $awaited = [
+            [
+                'id'        => '42',
+                'name'      => 'Talent',
+                'last_name' => 'Billy',
+                'age'       => '35',
+                'street'    => 'Elm Street'
+            ],
+            [
+                'id'        => '52',
+                'name'      => 'Duck',
+                'last_name' => 'Donald',
+                'age'       => '8',
+                'street'    => 'Duckcity'
+            ]
+        ];
+        $this->assertEquals($awaited, $result);
+
+    }
+
+    public function test_count_returns_count_of_simple_file()
+    {
+        $reader = $this->newReader($this->dataFile('simple-pipe-placeholder-normalized.csv'));
+        $this->assertCount(2, $reader);
+
+    }
+
+    public function test_count_returns_count_of_simple_file_without_header()
+    {
+        $reader = $this->newReader($this->dataFile('simple-pipe-placeholder-no-header.csv'));
+        $this->assertCount(2, $reader);
+
+    }
+
+    public function test_count_returns_count_of_country_file()
+    {
+        $reader = $this->newReader($this->dataFile('Countries-ISO-3166-2.csv'));
+        $this->assertCount(268, $reader);
+    }
+
+    public function test_count_returns_count_of_country_file_with_skippable_lines()
+    {
+        $reader = $this->newReader($this->dataFile('Countries-ISO-3166-2-semicolon-blank-lines.csv'));
+        $this->assertCount(268, $reader);
+    }
+
+    protected function newReader($path='', FSContract $filesystem=null)
+    {
+        return new CsvReadIterator($path, $filesystem);
+    }
+
+}
