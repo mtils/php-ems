e26f90457d210257d9ae47e35bb9fbc3e3fb386f
Michael Tils <mtils@web-utils.de>
2017-11-29 17:41:31 +0100

    Merged XType\ValueFormatter and XType\ValueFormatter

--- src/Ems/Contracts/XType/Formatter.php
+++ src/Ems/Contracts/XType/Formatter.php
@@ -2,7 +2,20 @@
 
 namespace Ems\Contracts\XType;
 
-interface Formatter
+use Ems\Contracts\Core\Extendable;
+
+/**
+ * Interface Formatter
+ *
+ * This is the XType counterpart of Core\Formatter. You can pass an xtype and
+ * the value gets formatted by the type.
+ *
+ * The extendable interface works with classes, not strings. So if you extend it
+ * for class Core\UnitType, every derived class will use that extension too.
+ *
+ * @package Ems\Contracts\XType
+ */
+interface Formatter extends Extendable
 {
     /**
      * Format the $path of $object fir $view in $lang.
@@ -12,9 +25,20 @@ interface Formatter
      * @param object $object
      * @param string $path   The path. Can be a property name or a nested path
      * @param string $view   (optional)
-     * @param string $lang   (optional)
      *
      * @return string
      **/
-    public function format($object, $path, $view = 'default', $lang = null);
+    public function format($object, $path, $view = 'default');
+
+    /**
+     * Format a single value by passing the XType by your own.
+     *
+     * @param XType  $type
+     * @param mixed  $value
+     * @param string $view
+     *
+     * @return string
+     **/
+    public function value(XType $type, $value, $view = 'default');
+
 }
