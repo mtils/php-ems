4094c066b702dffe4bbb7ebaee848f7f96ac4313
Michael Tils <mtils@web-utils.de>
2018-05-30 19:55:26 +0200

    Added test stub

---
+++ tests/integration/XType/FormatterIntegrationTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *  * Created by mtils on 26.05.18 at 08:15.
+ **/
+
+namespace Ems\XType;
+
+
+use Ems\IntegrationTest;
+
+class FormatterIntegrationTest extends IntegrationTest
+{
+    public function test_bogus()
+    {
+
+    }
+}
